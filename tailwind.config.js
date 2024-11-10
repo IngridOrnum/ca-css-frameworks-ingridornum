ee5e648b5862451e71e3595bd78461d2826421dc
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-11-10 16:05:22 +0100

    Chore: Added two cards on profile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,12 @@ module.exports = {
         'new-black': '#2F2F2F',
         'new-white': '#FCFCFB',
         'opacity-new-white': '#FCFCFB60'
-      }
+      },
+      backgroundImage: {
+        'profile-bg-gradient': 'linear-gradient(135deg, #F3D0BB 0%, #FCFCFB 75.14%)',
+        'welcome-profile-card': 'linear-gradient(293deg, #F3D0BB 12.68%, rgba(244, 244, 244, 0.30) 75.55%)',
+        'stats-profile-card': 'linear-gradient(128deg, #F3D0BB 25.8%, #FFFEFD 100%)'
+      },
     },
   },
   plugins: [],
