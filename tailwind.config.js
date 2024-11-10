f5d2f9c689a8f877078e180d2eaab964d5f013b8
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-11-10 22:51:52 +0100

    Chore: Added all cards in the profile page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,15 @@ module.exports = {
       backgroundImage: {
         'profile-bg-gradient': 'linear-gradient(135deg, #F3D0BB 0%, #FCFCFB 75.14%)',
         'welcome-profile-card': 'linear-gradient(293deg, #F3D0BB 12.68%, rgba(244, 244, 244, 0.30) 75.55%)',
-        'stats-profile-card': 'linear-gradient(128deg, #F3D0BB 25.8%, #FFFEFD 100%)'
+        'stats-profile-card': 'linear-gradient(128deg, #F3D0BB 25.8%, #FFFEFD 100%)',
+        'latest-post': 'linear-gradient(169deg, rgba(73, 30, 30, 0.39) 0%, #FCFCFB 100%)'
       },
+      height: {
+        'profile-post': '180px'
+      },
+      width: {
+        'profile-post': '300px'
+      }
     },
   },
   plugins: [],
