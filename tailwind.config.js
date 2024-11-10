3cd9d68da099eb979c6fe2286ceb2003e5c68e97
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-11-10 12:41:05 +0100

    Chore: Added login and register form. Added some icons in assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        custom: ['AdvercaseFont', 'sans-serif']
+        heading: ['AdvercaseFont', 'sans-serif'],
+        primary: ['Gotu', 'sans-serif'],
+        secondary: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'primary': '#2B1E49',
+        'secondary': '#F3D0BB',
+        'tertiary': '#EF6570',
+        'new-black': '#2F2F2F',
+        'new-white': '#FCFCFB',
+        'opacity-new-white': '#FCFCFB60'
       }
     },
   },
