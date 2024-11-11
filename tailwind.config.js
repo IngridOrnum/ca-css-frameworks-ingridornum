58ddc1a8bcc5747c04de2cc03c9d0d8e7319fb55
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-11-11 20:07:39 +0100

    Chore: Added side nav bar and fixed centering of content on profile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         'opacity-new-white': '#FCFCFB60'
       },
       backgroundImage: {
-        'profile-bg-gradient': 'linear-gradient(135deg, #F3D0BB 0%, #FCFCFB 75.14%)',
+        'profile-bg-gradient': 'linear-gradient(180deg, #FCFCFB 0%, #F3D0BB 100%)',
         'welcome-profile-card': 'linear-gradient(293deg, #F3D0BB 12.68%, rgba(244, 244, 244, 0.30) 75.55%)',
         'stats-profile-card': 'linear-gradient(128deg, #F3D0BB 25.8%, #FFFEFD 100%)',
         'latest-post': 'linear-gradient(169deg, rgba(73, 30, 30, 0.39) 0%, #FCFCFB 100%)'
